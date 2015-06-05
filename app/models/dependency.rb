f702d22ac2e2f8326c1bd30f79008d94d6dc5c0c
areainf <areainf.fce.unrc@gmail.com>
2015-06-05 17:00:49 -0300

    Asign master unit and view subdependencies en show

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -8,4 +8,17 @@ class Dependency < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: {:scope => :master_unit, :case_sensitive => false }
 
+  before_save :set_master_units
+
+  def set_master_units
+    if master_unit.blank?
+      par = parent
+      while !par.blank? && par.master_unit.blank?
+        par = par.parent
+      end
+      self.master_unit = par.master_unit unless par.blank?
+    end
+  
+  end
+
 end
